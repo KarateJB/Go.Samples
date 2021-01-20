66fd790bcecba6169d44d6aadd020d92a5354cdf
KarateJB <ppippenkimo@gmail.com>
2021-01-20 11:15:05 +0800

    Use rand to return random greeting msg

--- Samples/02.CreateModule/greetings/greetings.go
+++ Samples/02.CreateModule/greetings/greetings.go
@@ -3,6 +3,8 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 // Hello returns a welcome message for the named person.
@@ -11,6 +13,23 @@ func Hello(name string) (string, error) {
 		return "", errors.New("Empty name")
 	}
 
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	// message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// init sets the seed of rand
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	// A slice of message formats
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
