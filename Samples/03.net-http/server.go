4c1a77068f8499acb06d2917cac615c27e2cd68b
KarateJB <ppippenkimo@gmail.com>
2021-01-21 14:19:44 +0800

    Add counter page

--- Samples/03.net-http/server.go
+++ Samples/03.net-http/server.go
@@ -6,8 +6,12 @@ import (
 	"net/http"
 )
 
+var counter int
+
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", handlerCounter)  // E.q. /count
+	http.HandleFunc("/count/", handlerCounter) // E.q. /count/xxxx
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
@@ -15,4 +19,9 @@ func handler(rw http.ResponseWriter, req *http.Request) {
 	rw.WriteHeader(http.StatusOK)
 	// rw.Write([]byte(`My website`))
 	fmt.Fprintf(rw, "URL.Path = %q\n", req.URL.Path)
+}
+
+func handlerCounter(rw http.ResponseWriter, req *http.Request) {
+	counter++
+	fmt.Fprintf(rw, "Counter = %d\n", counter)
 }
