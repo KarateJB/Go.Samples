f497786e1ced4ff40c894dc34e41931af31cb1de
karatejb <ppippenkimo@gmail.com>
2022-05-23 04:50:24 +0800

    Add more API types

--- sample/04.gin/webservice-gorm/types/api/types.go
+++ sample/04.gin/webservice-gorm/types/api/types.go
@@ -1,13 +1,51 @@
 package types
 
-import "github.com/google/uuid"
+import (
+	"database/sql"
+	"time"
 
-type Todo struct {
-	Id     uuid.UUID `json:"id"`
-	Title  string    `json:"title"`
-	IsDone bool      `json:"isDone"`
+	"github.com/google/uuid"
+)
+
+type TrackDateTimes struct {
+	CreateOn time.Time    `json:"createOn"`
+	UpdateOn sql.NullTime `json:"updateOn"`
+	DeleteOn sql.NullTime `json:"deleteOn"`
 }
 
+type User struct {
+	Id    string `json:"id"`
+	Name  string `json:"name"`
+	Todos []Todo `json:"todos"`
+}
+
+type Todo struct {
+	Id             uuid.UUID `json:"id"`
+	Title          string    `json:"title"`
+	IsDone         bool      `json:"isDone"`
+	TrackDateTimes `json:"trackDateTimes"`
+	TodoExt        TodoExt `json:"todoExt"`
+	UserId         string  `json:"userId"`
+	Tags           []Tag   `json:"tags"`
+}
+
+type TodoExt struct {
+	Id          uuid.UUID `json:"id"`
+	Description string    `json:"description"`
+	Priority    Priority  `json:""`
+}
+
+type Tag struct {
+	Id   uuid.UUID `json:"id"`
+	Name string    `json:"name"`
+}
+
+type Priority struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+// TODO: Remove this struct
 type TodoPageData struct {
 	PageTitle string `json:"pageTitle"`
 	Todos     []Todo `json:"todos"`
