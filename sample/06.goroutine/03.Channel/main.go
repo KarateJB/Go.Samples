ac2c3628717d7ee1a0aeeaac2f7cb500c65deecf
karatejb <ppippenkimo@gmail.com>
2022-05-27 14:16:02 +0800

    Refine

---
+++ sample/06.goroutine/03.Channel/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	ch := make(chan int, 2)
+	go write(ch)
+
+	time.Sleep(2 * time.Second)
+
+	// for v := range ch {
+	// 	fmt.Println("Read value", v, "from channel")
+	// 	time.Sleep(2 * time.Second)
+	// }
+
+	// Or iterate channel like this
+	// LOOP1:
+	// 	for {
+	// 		v, ok := <-ch
+	// 		if ok {
+	// 			fmt.Println("Read value", v, "from channel")
+	// 			time.Sleep(2 * time.Second)
+	// 		} else {
+	// 			break LOOP1
+	// 		}
+	// 	}
+
+LOOP2:
+	for {
+		select {
+		case v, ok := <-ch:
+			if ok {
+				fmt.Println("Read value", v, "from channel")
+				time.Sleep(2 * time.Second)
+			} else {
+				break LOOP2
+			}
+		default:
+			fmt.Println("Channel blocking...")
+			// Do something...
+		}
+	}
+}
+
+func write(ch chan int) {
+
+	for i := 1; i <= 4; i++ {
+		time.Sleep(2 * time.Second)
+		ch <- i
+		fmt.Println("Successfully wrote", i, "to channel")
+	}
+	close(ch)
+}
